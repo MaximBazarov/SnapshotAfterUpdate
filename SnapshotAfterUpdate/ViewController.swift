bb0d3adec8656a6f41fc4cfd1076972531701929
Maxim Bazarov <maxim.bazarov@zalando.de>
2020-02-11 11:41:12 +0100

    Initial Commit

---
+++ SnapshotAfterUpdate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotAfterUpdate
+//
+//  Created by Maxim Bazarov on 2/11/20.
+//  Copyright © 2020 Maxim Bazarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
